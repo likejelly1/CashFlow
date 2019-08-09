31ee3bc0e2233cd05901380940c2c85661cc5ebb
likeJelly <jalluramadhan@gmail.com>
2019-08-09 14:49:52 +0700

    yuhu

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['nama_role' => 'sales']);
+        Role::create(['nama_role' => 'admin']);
+        Role::create(['nama_role' => 'finance']);
+        Role::create(['nama_role' => 'management']);
+        Role::create(['nama_role' => 'presales']);
+        Role::create(['nama_role' => 'PM']);
+        
+    }
+}
