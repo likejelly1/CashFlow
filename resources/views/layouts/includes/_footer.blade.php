bdc7fc4d557cbf287a3f850f1b9e6641bdf10050
priskaput <46365650+priskaput@users.noreply.github.com>
2019-08-09 16:44:34 +0700

    ini template home

---
+++ resources/views/layouts/includes/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="pull-right" style="color: #333;">
+            PT. MITRA INTI BERSAMA by <a href="https://colorlib.com">Colorlib</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
